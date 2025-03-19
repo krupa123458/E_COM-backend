d150f93df9caa4cc12b8feaf32a4d8e11875d8e3
krupa123458 <krupaganiger33@gmail.com>
2025-03-19 15:01:16 +0530

    second change

--- Server.js
+++ Server.js
@@ -13,7 +13,8 @@ connectDB()
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-const port=3000
+const port=7008
+
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`)
 })
